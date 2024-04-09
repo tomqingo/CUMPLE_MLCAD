53e74b32e46f7c11a035006c467dab4a0764f208
LUO Qin <qluo22@proj53.cse.cuhk.edu.hk>
2024-04-09 13:21:45 +0800

    update_runtime_analysis

--- scripts/statistics.py
+++ scripts/statistics.py
@@ -5,16 +5,40 @@ import math
 
 def get_cumple_results(fn):
     time = 0
-    wl = 0
     if not os.path.isfile(fn):
-        return 0, 0
+        return 0
     with open(fn) as f:
         lines = f.readlines()
         line = lines[-1].strip()
-        if "Total HPWL" in line:
+        if "Total HPWL" in line or "Total runtime" in line:
             time = float(line.split()[-2])
-            wl =  float(line.split()[3])
-    return time, wl
+    return time
+
+def get_vivado_PnR_time(fn):
+    print("=================", fn, "=================")
+    place_time = 0
+    route_time = 0
+    if not os.path.isfile(fn):
+        return 0, 0
+    with open(fn) as f:
+        for line in f.readlines():
+            if "place_design: Time (s): " in line:
+                line_col = line.strip().split()
+                time_str = line_col[9]
+                time_col = time_str.split(":")
+                hour = int(time_col[0])
+                minute = int(time_col[1])
+                second = int(time_col[2])
+                place_time = hour * 3600 + minute * 60 + second
+            elif "route_design: Time (s): " in line:
+                line_col = line.strip().split()
+                time_str = line_col[9]
+                time_col = time_str.split(":")
+                hour = int(time_col[0])
+                minute = int(time_col[1])
+                second = int(time_col[2])
+                route_time = hour * 3600 + minute * 60 + second
+    return place_time, route_time
 
 def get_vivado_scores(fn):
     print("=================", fn, "=================")
@@ -25,10 +49,6 @@ def get_vivado_scores(fn):
     with open(fn) as f:
         start = False
         start2 = False
-        date_start = 0
-        date_end = 0
-        time_start = 0
-        time_end = 0
         dirs = ["NORTH", "SOUTH", "EAST", "WEST"]
         max_level = 0
         for line in f.readlines():
@@ -57,22 +77,6 @@ def get_vivado_scores(fn):
             if start2:
                 if "Number of Nodes with overlaps" in line:
                     detailed_score += 1
-            
-            if "Start of session at" in line:
-                time_col = line.split()
-                date_start = int(time_col[7])
-                hour_min_col = time_col[8].split(':')
-                time_start = int(hour_min_col[0]) + int(hour_min_col[1])*1.0/60 + (int(hour_min_col[2])*1.0)/3600
-
-            if "Exiting Vivado" in line:
-                time_col = line.split()
-                date_end = int(time_col[8])
-                hour_min_col = time_col[9].split(':')
-                time_end = int(hour_min_col[0]) + int(hour_min_col[1])*1.0/60 + (int(hour_min_col[2])*1.0)/3600
-            
-        vivado_time = (time_end - time_start) + (date_end - date_start)*24
-
-            
 
     if len(init_scores) == 0:
         print("No congested area !!!")
@@ -80,15 +84,12 @@ def get_vivado_scores(fn):
     else:
         print("Init scores:", init_scores)
 
-    # 1.2 x sumi(max(0, short_congestion_leveli-3)2) + sumi(max(0, global_congestion_leveli-3)2), where i=north, south, east, and south.
     init_score = 1
     for i in range(len(init_scores)):
         coef = 1.0
-        # if i % 2 == 1:
-        #     coef = 1.2
         init_score += coef * max(0, init_scores[i] - 3) ** 2
     print("Init_Score:", init_score, "Detailed_Score", detailed_score)
-    return init_score, detailed_score, vivado_time
+    return init_score, detailed_score
 
 
 if __name__ == "__main__":
@@ -105,12 +106,19 @@ if __name__ == "__main__":
     for design in designs:
         design_path = os.path.join(run_name, design)
         cumple_log_fn = os.path.join(design_path, "run.log")
-        time, wl = get_cumple_results(cumple_log_fn)
+        macro_place_time = get_cumple_results(cumple_log_fn)
 
         vivado_log_fn = os.path.join(design_path, "vivado.log")
-        init_score, detailed_score, vivado_time = get_vivado_scores(vivado_log_fn)
-        final_score = init_score * detailed_score * (1 + max(1.22*time - 600, 0) / 60) * 1.22*vivado_time
-        res.append(["Design_{}".format(design[1:]), wl, time, vivado_time, init_score, detailed_score, final_score])
-    df = pd.DataFrame(res, columns=["Design", "HPWL", "Time", "Vivado_Time", "Init_Score", "Detailed_Score", "Final_Score"])
+        LUTFF_place_time, route_time = get_vivado_PnR_time(vivado_log_fn)
+        init_score, detailed_score = get_vivado_scores(vivado_log_fn)
+
+        total_place_time = macro_place_time + LUTFF_place_time
+        PnR_time = total_place_time + route_time
+        
+        congest_score = init_score + detailed_score
+        res.append(["Design_{}".format(design[1:]), init_score, detailed_score, congest_score, total_place_time, route_time, PnR_time])
+    
+    df = pd.DataFrame(res, columns=["Design", "Init_Score", "Detailed_Score", "Congest_Score", "Place_time", "Route_time", "PnR_time"])
+    
     print(df)
     df.to_csv(os.path.join(run_name, "stat.csv"), index=None)
